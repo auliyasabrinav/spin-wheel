b55e2dbff9df34fb7bfa767c1bdf4928c70321b0
Auliya Sabrina <155187261+auliyasabrinav@users.noreply.github.com>
2026-07-28 11:08:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,8 @@ st.set_page_config(
     layout="wide"
 )
 
+with st.sidebar:
+    st.title("📋 Menu")
+    st.write("Testing Sidebar")
+
 st.title("🎉 SPIN WHEEL DOORPRIZE")
-
-st.markdown("---")
-
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-    st.metric("Peserta", 0)
-
-with col2:
-    st.metric("Prize", 0)
-
-with col3:
-    st.metric("Sudah Menang", 0)
-
-with col4:
-    st.metric("Sisa Peserta", 0)
-
-st.success("Selamat datang di aplikasi Spin Wheel Doorprize.")
